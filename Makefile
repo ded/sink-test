ef4e90a4e384e314a834b8fdaf37739d084886d5
Dustin Diaz <dustin@dustindiaz.com>
2011-03-07 02:25:01 -0800

    refactor makefile into node execs

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
 test:
-	@echo Running Sink Tests...
 	node make/make.js
-	node make/run.js
-	node make/clean.js
--- make/make.js
+++ make/make.js
@@ -1,5 +1,11 @@
 var fs = require('fs');
+var exec = require('child_process').exec;
 
 var headless = fs.readFileSync('src/headless.js');
 var tests = fs.readFileSync('tests.js');
 fs.writeFileSync('make/run.js', headless + tests);
+exec('node make/run.js', function (err, out) {
+  console.log(out);
+  var fs = require('fs');
+  fs.unlink('make/run.js');
+});
